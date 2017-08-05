68ce4aaca330c3b6c8aac4e03bbdf6ec96274dc6
Bruce Conner <bruce.conner.ii@gmail.com>
2017-08-05 08:46:47 -0400

    Konami Code
    Adding in versions of the code that finish on the enter key and on the b
    key to go along with my original version that ends with the space key.

--- konamiCode.js
+++ konamiCode.js
@@ -1,3 +1,4 @@
+//Activate with the space key as the last key
 function konamiCode () {
 	var validKeys = {
 		32: 'space',
@@ -10,6 +11,67 @@ function konamiCode () {
 	};
 
 	var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'space'];
+	var konamiCodePosition = 0;
+
+	document.addEventListener('keydown', function(e) {
+		var keyPressed = validKeys[e.keyCode];
+		var requiredKey = konamiCode[konamiCodePosition];
+
+		if (keyPressed == requiredKey) {
+			konamiCodePosition++;
+
+			if (konamiCodePosition == konamiCode.length) {
+				//Konami Code activated, do code stuff here
+			}
+		} else {
+			konamiCodePosition = 0;
+		}
+	});
+};
+
+//Activate with the enter key as the last key
+function konamiCode () {
+	var validKeys = {
+		13: 'enter',
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down',
+		65: 'a',
+		66: 'b'
+	};
+
+	var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'enter'];
+	var konamiCodePosition = 0;
+
+	document.addEventListener('keydown', function(e) {
+		var keyPressed = validKeys[e.keyCode];
+		var requiredKey = konamiCode[konamiCodePosition];
+
+		if (keyPressed == requiredKey) {
+			konamiCodePosition++;
+
+			if (konamiCodePosition == konamiCode.length) {
+				//Konami Code activated, do code stuff here
+			}
+		} else {
+			konamiCodePosition = 0;
+		}
+	});
+};
+
+//Activate with the a key as the last key
+function konamiCode () {
+	var validKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down',
+		65: 'a',
+		66: 'b'
+	};
+
+	var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 	var konamiCodePosition = 0;
 
 	document.addEventListener('keydown', function(e) {
